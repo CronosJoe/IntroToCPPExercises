e1a0bec0a410009f85fedc565234a5c991355d3b
Joseph Huskey <s209062@students.aie.edu>
2020-11-18 15:09:33 -0800

    Almost done with arrays for the second time need to get water

--- CPPArrayExercisesRedo/CPPArrayExercisesRedo/CPPArrayExercisesRedo.cpp
+++ CPPArrayExercisesRedo/CPPArrayExercisesRedo/CPPArrayExercisesRedo.cpp
@@ -2,10 +2,73 @@
 //
 
 #include <iostream>
-
+void printNumbers(int numbers[], size_t size) 
+{
+    for(int i = 0; i<size; ++i)
+    {
+        std::cout << numbers[i]<<std::endl;
+    }
+}
+int sumNumbers(int numbers[], size_t size)
+{
+    int sum = 0;
+    for(int i =0; i<size; ++i)
+    {
+        sum += numbers[i];
+    }
+    return sum;
+}
+int largestValue(int numbers[], size_t size)
+{
+    int largest = INT32_MIN;
+    for(int i = 0; i<size; ++i)
+    {
+        if(numbers[i]>largest)
+        {
+            largest = numbers[i];
+        }
+    }
+    return largest;
+}
+int findValue(int numbers[], size_t size, int value, int start)
+{
+    int index = -1; //this is the default return in case it fails.
+    for(int i = start; i<size;++i)
+    {
+        if(value == numbers[i])
+        {
+            return i;
+        }
+    }
+    return index;
+}
+int countElement(int numbers[], size_t size, int value)
+{
+    int count = 0;
+    for(int i=0; i<size; ++i)
+    {
+        if(numbers[i]==value)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int numb[] = { 1,2,3,4};
+    int arrSize = sizeof(numb) / sizeof(numb[0]);
+    //printNumbers(numb, arrSize);
+    int sum = sumNumbers(numb, arrSize);
+    //std::cout << sum << std::endl;
+    int largest = largestValue(numb, arrSize);
+    //std::cout << largest << std::endl;
+    int foundIndex = findValue(numb, arrSize, 2, 0);
+    //std::cout << foundIndex << std::endl;
+    int occurance = countElement(numb, arrSize, 1);
+    //std::cout << occurance << std::endl;
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
