c50f9cbc54070c304bbbc5a515a38cb2833d59e2
Joseph Huskey <s209062@students.aie.edu>
2020-11-19 12:40:58 -0800

    almost done with pointers exercises

--- CPPPointersExercises/CPPPointersExercises/CPPPointersExercises.cpp
+++ CPPPointersExercises/CPPPointersExercises/CPPPointersExercises.cpp
@@ -9,6 +9,13 @@ void printFloats(float* arr, int size)
         std::cout << arr[i] << std::endl;
     }
 }
+void printInt(int* arr, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << std::endl;
+    }
+}
 int arraySum(int* arr, int size)
 {
     int sum = 0;
@@ -18,6 +25,45 @@ int arraySum(int* arr, int size)
     }
     return sum;
 }
+void initBools(bool * arr, int size, bool defaultValue)
+{
+    for(int i =0; i<size; ++i)
+    {
+        arr[i] = defaultValue;
+    }
+}
+int* zeroArray(size_t size)
+{
+  
+    int* dynZero = new int [size];
+    for(int i = 0; i<size; ++i)
+    {
+       dynZero[i] = 0;
+    }
+    return dynZero;
+}
+void favoriteNumbers()
+{
+    int maxNum, n;
+    int* p;
+    std::cout << "How many numbers would you like to type? ";
+    std::cin >> maxNum;
+    p = new int[maxNum];
+    
+        for (n = 0; n < maxNum; n++)
+        {
+            std::cout << "what is one of your favorite numbers?: ";
+            std::cin >> p[n];
+        }
+        std::cout << "Your number(s): ";
+        for (n = 0; n < maxNum; n++)
+        {
+            std::cout << p[n] << ", ";
+        }
+        delete[] p;
+    
+}
+
 int main()
 {
     float numbers[] = { 1.2,1.3,1.4,1.5 };
@@ -29,6 +75,13 @@ int main()
     int sum = arraySum(numbs, intArrSize);
     //std::cout << sum << std::endl;
 
+    bool check[5];
+    initBools(check, 5, false);
+//  std::cout << "Check" << std::endl; used for debug stop points
+    int* dynZero = zeroArray(5);
+   // printInt(dynZero, 5);
+    delete[] dynZero;
+    favoriteNumbers();
 
 }
 
